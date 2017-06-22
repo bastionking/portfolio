e9ab03c9591267fd2826053f9ad3b2d1d61b516b
bastionking <bastionking@hotmail.my>
2017-06-22 14:37:03 +0800

    Integrated new and create action for portfolios

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -2,4 +2,23 @@ class InfosController < ApplicationController
   def index
   @portfolio_items = Info.all
   end
+
+  def new
+    @portfolio_items = Info.new
+  end
+
+  def create
+      @portfolio_items = Info.new(params.require(:info).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to infos_path, notice: 'Portfolio is now live.' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end

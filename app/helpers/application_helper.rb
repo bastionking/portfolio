3ca5966f395ed0d3091804a2c3519f5de2d7287e
bastionking <bastionking@hotmail.my>
2017-07-01 11:57:39 +0800

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,8 +17,45 @@ module ApplicationHelper
     end
   end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },
+      {
+        url: infos_path,
+        title: 'Info'
+      }
+    ]
+  end
+
   def copyright_generator
    DevcampViewTool::Renderer.copyright 'David Jian', 'All rights reserved'
   end
 
+  def nav_helper(style, tag_type)
+    nav_links = ''
+
+    nav_items.each do |item|
+    nav_links <<  "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    "active" if current_page? path
+  end
 end

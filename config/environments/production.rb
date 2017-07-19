369def697ff59b92b951520e04eabe0fd53d2dfb
bastionking <bastionking@hotmail.my>
2017-07-19 11:39:39 +0800

    Integrated action calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://davidj-portfolio.herokuapp.com/', 'http://davidj-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://davidj-portfolio.herokuapp.com/cable"
 end
